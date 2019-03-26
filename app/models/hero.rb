66930b8fec65a78071c0c49790047c3b03d6029a
Tom Holmes <tholmes59@gmail.com>
2019-03-26 00:08:50 +0000

    Automatically backed up by Learn

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -2,10 +2,17 @@ class Hero
   
   attr_accessor :name, :power, :bio
   
+  @@heros = []
+  
   def initialize(args)
     @name = args[:name]
     @power = args[:power]
     @bio = args[:bio]
+    @@heros << self
+  end 
+  
+  def self.all 
+    @@heros
   end 
   
 end 
