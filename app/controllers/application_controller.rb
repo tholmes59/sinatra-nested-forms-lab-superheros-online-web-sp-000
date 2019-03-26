bb0576f4dd8976cddbd344b0f8728523f5c50597
Tom Holmes <tholmes59@gmail.com>
2019-03-26 00:01:08 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class App < Sinatra::Base
   end 
   
   post '/teams' do
-     @team = params[:team]
+     @team = Team.new.(params[:team])
      
      params[:team][:members].each do |details|
        Hero.new(details)
